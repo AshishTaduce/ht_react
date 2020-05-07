2f66656b0fc98615ca80554f5ca862fde0ca794f
AshishTaduce <ashishteddy2@gmail.com>
2020-05-07 16:22:53 +0530

    Updates

---
+++ precache-manifest.3420fdba60dadfc990fed338d82c279a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a50fb1aebefdf18ce71ee78edbb0126",
+    "url": "/ht_react/index.html"
+  },
+  {
+    "revision": "1e07fbf07153d6811587",
+    "url": "/ht_react/static/css/main.6cdc9e2d.chunk.css"
+  },
+  {
+    "revision": "1eb3590d4f23126aa4e7",
+    "url": "/ht_react/static/js/2.c4fe823c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ht_react/static/js/2.c4fe823c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e07fbf07153d6811587",
+    "url": "/ht_react/static/js/main.2615f295.chunk.js"
+  },
+  {
+    "revision": "e614231463cee04bd278",
+    "url": "/ht_react/static/js/runtime-main.1ef871cb.js"
+  },
+  {
+    "revision": "6843fce189ed9ec0f6d3bf6c8debaf4f",
+    "url": "/ht_react/static/media/comment-24px.6843fce1.svg"
+  }
+]);
